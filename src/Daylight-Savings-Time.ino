1640cb3393b6d22e71a348acd56ea6267567249f
Charles McClelland <chip@mcclellands.org>
2019-11-12 09:03:07 -0500

    Update to better handle date changes

--- src/Daylight-Savings-Time.ino
+++ src/Daylight-Savings-Time.ino
@@ -13,33 +13,51 @@
 float Timezone = -5.0;                                // Example Eastern Standard Time
 float DSToffsetValue = 1.0;                           // Example EST - can set to zero for states that do not observice DST
 
+unsigned long januaryMidnight = 1546318800;
+unsigned long februaryFifteenthMidnight = 1550188800;
+unsigned long halloweenMidnight = 1572494400;
+unsigned long hourSeconds = 60*60;
+unsigned long daySeconds = 24 * 60 * 60;
+time_t testTime;
+
+
 // These are UTC time test values that are based on the example EST timezone
-const int numberOfTestPoints = 10;
-const unsigned long testTimesAndDates[numberOfTestPoints] = {1547532000,1551592800,1552629600,1560578400,1571119200,1572588000,1572739200,1572760800,1572782400,1576389600 };
 char data[128];     // Where we will store the output to Publish
 
 
 void setup() {
+  Serial.begin();
   Time.zone(Timezone);
   Time.setDSTOffset(DSToffsetValue);
 }
 
 
 void loop() {
-  static unsigned int i=0;
-  Time.setTime(testTimesAndDates[i]);                                           // Note this is UTC time not local.  
-  // Now, we will see if the Timezone calculations are correct
-  if (Time.isValid()) 
-  { // We only want to run this if time is valid - otherwise random result
-    isDSTusa() ? Time.beginDST() : Time.endDST();       
+  for (int i=0; i<=30; i++) {
+    testTime = februaryFifteenthMidnight + i * daySeconds;
+    Time.setTime(testTime);                                           // Note this is UTC time not local.  
+
+    // Now, we will see if the Timezone calculations are correct
+    if (Time.isValid()) 
+    { // We only want to run this if time is valid - otherwise random result
+      isDSTusa() ? Time.beginDST() : Time.endDST();       
+    }
+    snprintf(data, sizeof(data),"%s - DST: %s", (const char*)Time.timeStr(), Time.isDST() ? "True" : "False");
+    Serial.println(data);
   }
-  snprintf(data, sizeof(data),"The time is %s and the TimeZone is %s", (const char*)Time.timeStr(), Time.isDST() ? "True" : "False");
-  Particle.publish("Test",data,PRIVATE);
-  i++;                                                                         // Move to the next Test Case
-  delay(1000);                                                                 // Observe Particle Rate Limits
-  if (i > numberOfTestPoints) {                                                // Done - stop iterating to save data
-    while (1) Particle.process();                                              // Keep from going unresponsive as no more main loop transits
+
+  for (int i=0; i<=30; i++) {
+    testTime = halloweenMidnight + i * daySeconds;
+    Time.setTime(testTime);                                           // Note this is UTC time not local.  
+    // Now, we will see if the Timezone calculations are correct
+    if (Time.isValid()) 
+    { // We only want to run this if time is valid - otherwise random result
+      isDSTusa() ? Time.beginDST() : Time.endDST();       
+    }
+    snprintf(data, sizeof(data),"%s - DST: %s", (const char*)Time.timeStr(), Time.isDST() ? "True" : "False");
+    Serial.println(data);
   }
+  while (1) Particle.process();                                              // Keep from going unresponsive as no more main loop transits
 }
 
 
@@ -61,20 +79,12 @@ bool isDSTusa() {
     return false;
   }
 
-  // So, only March and November are left - time to look at days
-  // November first
-  boolean firstSundayOrAfter = (dayOfMonth - dayOfWeek > 0);
-  if (!firstSundayOrAfter)
-  { // before switching Sunday
-    return (month == 11); // November DST will be true, March not
-  }
+  boolean beforeFirstSunday = (dayOfMonth - dayOfWeek < 0);
+  boolean secondSundayOrAfter = (dayOfMonth - dayOfWeek > 7);
 
-  // Now March
-  boolean secondSundayOrAfter = (dayOfMonth - dayOfWeek > 8);
-  if (secondSundayOrAfter)
-  { // AFTER the switching Sunday
-    return (month == 3); // for March DST is true, for October not
-  }
+  if (beforeFirstSunday && !secondSundayOrAfter) return (month == 11);
+  else if (!beforeFirstSunday && !secondSundayOrAfter) return false;
+  else if (!beforeFirstSunday && secondSundayOrAfter) return (month == 3);
 
   int secSinceMidnightLocal = Time.now() % 86400;
   boolean dayStartedAs = (month == 10); // DST in October, in March not
